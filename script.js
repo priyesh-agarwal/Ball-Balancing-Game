7e478028fabd0279b820b6803fa92f1c063eaab9
priyesh-agarwal <46623774+priyesh-agarwal@users.noreply.github.com>
2020-06-10 18:33:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,108 +1,111 @@
-var canvas = document.getElementById("mycanvas");
-var ctx = canvas.getContext("2d");
-var x = canvas.width/2;
-var y = canvas.height-30;
-var dx = 1;
-var dy = -1.5;
-var ball_size = 8;
-var start=true;
-var baseX = 0;
-const baseY = canvas.height-10;
-var base_speed = 2;
-var moveRight = 0;
-var score = 0;
-function startGame(){
-    start = false;
-    document.getElementById("text").innerText = "";
-    document.getElementById("myscore").innerText = "";
-    x = canvas.width/2;
-    y = canvas.height-30;
-    dx = 1;
-    dy = -1.5;
-    ball_size = 8;
-    baseX = 0;
-    baseY = canvas.height-10;
-    base_speed = 2;
-    moveRight = 0;
-    score = 0;
-}
-function drawBall(){
-    ctx.beginPath();
-    ctx.arc(x,y,ball_size,0,Math.PI*2);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+let score = 0;
+let highest_score = 0;
+let SCORE = document.getElementById('score');
+const player = {
+    x: canvas.width/3,y: canvas.height-30,w: 200,h: 30,
+    dx: 0,speed: 7,
+    moveLeft: function(){
+        this.dx = -this.speed;
+    },
+    moveRight: function(){
+        this.dx = this.speed;
+    },
+    checkWall: function(){
+        if(this.x < 0){
+            this.x = 0;
+            //dx = 0;
+        }
+        if(this.x + this.w > canvas.width){
+            this.x = canvas.width - this.w;
+            //dx = 0;
+        }
+    },
+    newpos: function(){
+        this.x += this.dx;
+        this.checkWall();
+    },
+    draw: function(){
+        ctx.fillStyle = 'red';
+        ctx.fillRect(this.x,this.y,this.w,this.h);
+    },
     
-    if(x+ball_size>=canvas.width || x<=ball_size){
-        dx = -dx;
+};
+
+const ball = {
+    x: canvas.width/2,y: canvas.height/2,r: 20,
+    dx: 8,dy: 10,
+
+    bounce: function(){
+        // ball lands on bar
+        if((this.y + this.r + player.h > canvas.height) && (this.x >= player.x && this.x <= player.x+player.w)){
+            this.y = canvas.height - player.h - this.r;
+            this.dy = -this.dy;
+            score++;
+            if(highest_score < score){
+                highest_score = score;
+            }
+        }
+        // left wall
+        if(this.x < this.r){
+            this.x = this.r;
+            this.dx = -this.dx;
+        }
+        // right wall
+        if(this.x + this.r > canvas.width){
+            this.x = canvas.width - this.r;
+            this.dx = -this.dx;
+        }
+        // top wall
+        if(this.y < this.r){
+            this.y = this.r;
+            this.dy = -this.dy;
+        }
+        // ball falls down
+        if(this.y + this.r > canvas.height){
+            this.x = canvas.width/2;
+            this.y = canvas.height/4;
+            dx = 3,dy = 2;
+            player.dx = 0;
+            player.x = canvas.width/3;
+            score = 0;
+        }
+    },
+
+    newpos: function(){
+        if(player.dx != 0){
+            this.x += this.dx;
+            this.y += this.dy;
+            this.bounce();
+            
+        }
+    },
+
+    draw: function(){
+        ctx.fillStyle = 'blue';
+        ctx.beginPath();
+        ctx.arc(this.x,this.y,this.r,0,Math.PI*2,true);
+        ctx.fill();
     }
     
-    if(y+ball_size>=canvas.height){
-        gameOver();
-    }
-    if(y<=ball_size){
-        dy = -dy;
-    }
-    if(baseY-y==ball_size && (x>=baseX && x<=(baseX+100))){
-        if(dy>0)
-            dy = -dy;
-        score += 1;
-    }
-    if(start === false){
-    x += dx;
-    y += dy;
-    }
 }
 
-function gameOver(){
-    start = true;
-    document.getElementById("text").innerText = "You Lose!!";
-    document.getElementById("myscore").innerText = "Your score is "+score;
-    score = 0;
+function keyDown(e){
+    let s = e.key;
+    if(s === 'ArrowLeft') player.moveLeft();
+    if(s === 'ArrowRight') player.moveRight();   
 }
-
-function draw(){
-    if(start==false){
+function update(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
-    drawBall();
-    drawBase();
-    }
+    player.draw();
+    ball.draw();
+    //drawBall();
+    player.newpos();
+    ball.newpos();
+    SCORE.innerText = 'SCORE = ' + score + '     TOP SCORE = ' + highest_score;
+    requestAnimationFrame(update);
     
 }
-function moveleft(){
-    if(start==false)
-        moveRight = -1;
-}
-function moveright(){
-    if(start==false)
-        moveRight = 1;
-}
-function stop(){
-    if(start==false)
-        moveRight = 0;
-}
-function drawBase(){
-    ctx.beginPath();
-    ctx.rect(baseX,baseY,100,10);
-    ctx.fillStyle = "red";
-    ctx.fill();
-    ctx.closePath();
-    if(baseX<=0){
-        if(moveRight!=1)
-            moveRight=0;
-    }
-    if(baseX+100>=canvas.width){
-        if(moveRight!=-1)
-            moveRight = 0;
-    }
-    if(start==false){
-    if(moveRight===1){
-        baseX += base_speed;
-    }
-    if(moveRight===-1){
-        baseX -= base_speed;
-    }
-    
-    }
-}
-setInterval(draw,10);
+update();
+document.addEventListener('keydown',keyDown);
